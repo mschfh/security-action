132ba8ace3cdf98f1f8089be8b88ae70a0df4252
bcaller <bcaller@brave.com>
2023-01-26 16:11:41 +0100

    debugger

--- assets/debug.sh
+++ assets/debug.sh
@@ -6,3 +6,5 @@ tree -a $RUNNER_TEMP
 realpath "$0"
 realpath -s "$0"
 echo $ASSIGNEES
+find . -name Gemfile.lock | xargs -0 -n1 dirname | sort -u &&\
+      xargs -I{} ../.gem/bin/bundler exec brakeman --quiet --format tabs --no-exit-on-warn --no-exit-on-error {}
