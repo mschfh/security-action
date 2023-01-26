9a64e7bde199f6cc2331c1ad8c2e087a69968618
bcaller <bcaller@brave.com>
2023-01-26 16:44:04 +0100

    quiet

--- assets/debug.sh
+++ assets/debug.sh
@@ -18,10 +18,10 @@ echo json
 echo pathsandjson
 
 find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u &&\
-      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX\
+      xargs -IXXX ../.gem/bin/bundler exec brakeman --quiet --format json --no-exit-on-warn --no-exit-on-error XXX\
     
 echo pathsandjsonandjq
 
 find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u &&\
-      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX \
+      xargs -IXXX ../.gem/bin/bundler exec brakeman --quiet --format json --no-exit-on-warn --no-exit-on-error XXX \
       | jq -r '.warnings[] | "\(.file):\(.line) \(.message)<br><br>source: \(.link)<br><br>"'
