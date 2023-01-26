9a2611021cd2ca00b16c65f1b551e15f055a506e
bcaller <bcaller@brave.com>
2023-01-26 16:42:29 +0100

    alsjdflk3

--- assets/debug.sh
+++ assets/debug.sh
@@ -6,15 +6,21 @@ tree -a $RUNNER_TEMP
 realpath "$0"
 realpath -s "$0"
 echo $ASSIGNEES
-find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u &&\
-      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX \
-      | jq -r '.warnings[] | "\(.file):\(.line) \(.message)<br><br>source: \(.link)<br><br>"' \
-      | sort -u | sed "s/$/Cc @brave\/sec-team $ASSIGNEES/g"
+
+echo Paths
+
+find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u
+
+echo json
 
 ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error t3sts/brakeman
 
+echo pathsandjson
+
 find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u &&\
-      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX
+      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX\
+    
+echo pathsandjsonandjq
 
 find . -name Gemfile.lock | xargs dirname | grep -v Gemfile.lock | sort -u &&\
       xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX \
