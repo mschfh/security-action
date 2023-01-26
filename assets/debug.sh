d739ea31aca6da3e9f0524d54a53331eafd54906
bcaller <bcaller@brave.com>
2023-01-26 16:36:27 +0100

    fajsl

--- assets/debug.sh
+++ assets/debug.sh
@@ -7,6 +7,6 @@ realpath "$0"
 realpath -s "$0"
 echo $ASSIGNEES
 find . -name Gemfile.lock | xargs -0 -n1 dirname | grep -v Gemfile.lock | sort -u &&\
-      xargs -I{} ../.gem/bin/bundler exec brakeman --quiet --format json --no-exit-on-warn --no-exit-on-error {} \
+      xargs -IXXX ../.gem/bin/bundler exec brakeman --format json --no-exit-on-warn --no-exit-on-error XXX \
       | jq -r '.warnings[] | "\(.file):\(.line) \(.message)<br><br>source: \(.link)<br><br>"' \
       | sort -u | sed "s/$/Cc @brave\/sec-team $ASSIGNEES/g"
